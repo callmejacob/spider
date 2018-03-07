915fe9b72491d779ea42d6b9b89b8608a93c2dc0
fengshuxun <fengshuxun@baidu.com>
2018-03-07 20:00:34 +0800

    增加用户点击最多的标题统计

---
+++ HelloScrapy/DataParse/mysql_parser.py
@@ -0,0 +1,116 @@
+# -- coding: utf-8 --
+
+import mysql.connector
+import jieba
+import jieba.posseg as pseg
+import json
+import operator
+
+# 解决UnicodeWarning问题
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+HP_DESC = {
+	u'Ag' : '形语素',
+	u'n'  : '名词',
+	u'v'  : '动词',
+	u'x'  : '非语素字',
+	u'r'  : '代词',
+	u'm'  : '数词',
+	u'd'  : '副词',
+	u'a'  : '形容词',
+	u'nr' : '人名',
+	u'p'  : '介词',
+	u'c'  : '连词',
+	u't'  : '时间词',
+	u'ns' : '地名',
+	u'f'  : '方位词',
+	u'i'  : '成语',
+	u'l'  : '习用语',
+	u'vn' : '名动词',
+	u'y'  : '语气词',
+	u'u'  : '助词',
+	u'nz' : '其他专名',
+	u's'  : '处所词',
+	u'q'  : '量词',
+}
+
+def getHpDesc(hp):
+	if hp in HP_DESC:
+		return HP_DESC[hp]
+	else:
+		return hp
+
+def printHpItems(sorted_items, hp_desc, size=-1):
+	print hp_desc
+	print '---' * 20
+	count = 0;
+	for item in sorted_items:
+		word = item[0]
+		val = item[1]
+		hp = ''
+		if word in result_hp:
+			hp = result_hp[word]
+		if getHpDesc(hp) == hp_desc:
+			print '{:<10s}{:d}'.format(word, val)
+			if size > 0:
+				count += 1
+				if (count > size):
+					break
+	print '---' * 20
+	print '\n\n'
+
+# 初始化中文分词器
+jieba.initialize()
+
+# 打开数据库
+cnx = mysql.connector.connect(user='root', password='fengshuxun', database='jianshu')
+cursor = cnx.cursor()
+
+# 分条拉取
+result = {}
+result_hp = {}
+hp_stat = {}
+
+cursor.execute("SELECT * FROM article_desc ORDER BY title ASC")
+for title, author, time, link, abstract, readnum, reply_num, favor_num in cursor:
+	words = jieba.cut(title, cut_all = False)
+	for word in words:
+		if word in result:
+			result[word] += int(readnum)
+		else:
+			result[word] = int(readnum)
+
+	# 词性分析
+	words = pseg.cut(title)
+	for word, flag in words:
+		result_hp[word] = flag
+		if flag in hp_stat:
+			hp_stat[flag] += 1
+		else:
+			hp_stat[flag] = 1
+
+# 排序
+# sorted(result.items(),key = lambda x:x[1],reverse = True)
+sorted_items = sorted(result.items(),key = operator.itemgetter(1), reverse = True)
+sorted_hp = sorted(hp_stat.items(),key = operator.itemgetter(1), reverse = False)
+
+# 打印
+printHpItems(sorted_items, '名词', 30)
+printHpItems(sorted_items, '动词', 30)
+
+# for item in sorted_hp:
+# 	hp = item[0]
+# 	val = item[1]
+# 	print getHpDesc(hp), val
+
+# 记录result
+file = open('result.txt', 'w')
+line = json.dumps(result)
+file.write(line)
+file.close()
+
+# 关闭数据库
+cursor.close()
+cnx.close()
